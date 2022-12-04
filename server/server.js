379dbed6209da266b935b5a9c13cd4b88f662e6d
Gibson Han <66423127+gibsonliketheguitar@users.noreply.github.com>
2022-12-04 09:30:16 -0800

    add get companyprofile2 and refactor api calls to promises

--- server/server.js
+++ server/server.js
@@ -9,25 +9,54 @@ const api_key = finnhub.ApiClient.instance.authentications["api_key"];
 api_key.apiKey = "bv4mnbf48v6qpate9n30"; // Replace this
 const finnhubClient = new finnhub.DefaultApi();
 
-app.get("/stock", (req, res) => {
-  const { symbol } = req.query;
-  try {
+function getQuote(symbol) {
+  return new Promise((resolve, reject) => {
     finnhubClient.quote(symbol, (error, data, response) => {
-      if (error) throw new Error(error);
+      if (error) reject(error);
       else if (data.o === 0 && data.h === 0 && data.l === 0)
-        res.status(400).send("Bad Request");
+        reject("Invalid Symbol");
       else {
-        res.send({
-          symbol,
-          timeOfQuery: new Date(),
+        resolve({
           currentPrice: data.c,
           previousClose: data.pc,
           percentChange: data.dp,
         });
       }
     });
+  });
+}
+
+function getProfile(symbol) {
+  return new Promise((resolve, reject) => {
+    finnhubClient.companyProfile2({ symbol }, (error, data, response) => {
+      if (error) reject(error);
+      else {
+        resolve({
+          name: data.name,
+          logo: data.logo,
+          weburl: data.weburl,
+          industry: data.finnhubIndustry,
+        });
+      }
+    });
+  });
+}
+
+app.get("/stock", async (req, res) => {
+  const { symbol } = req.query;
+  try {
+    const profile = await getProfile(symbol);
+    const prices = await getQuote(symbol);
+
+    console.log(profile, prices);
+    res.status(200).send({
+      symbol,
+      timeOfQuery: new Date(),
+      ...profile,
+      ...prices,
+    });
   } catch (error) {
-    res.status(404).send("Server Failure");
+    res.status(404).send("Server Failure", error);
   }
 });
 

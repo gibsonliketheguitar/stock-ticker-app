b374b60b82d7ef012309b9786b5d02e1b2f6302f
Gibson Han <66423127+gibsonliketheguitar@users.noreply.github.com>
2022-12-04 06:34:38 -0800

    add finhubb package and method to get price information

--- server/server.js
+++ server/server.js
@@ -10,9 +10,31 @@ app.get("/", (req, res) => {
   res.send("Home Data");
 });
 
+const finnhub = require("finnhub");
+const api_key = finnhub.ApiClient.instance.authentications["api_key"];
+api_key.apiKey = "bv4mnbf48v6qpate9n30"; // Replace this
+const finnhubClient = new finnhub.DefaultApi();
+
 app.get("/stock", (req, res) => {
-  console.log(req.body);
-  res.send({ data: "stock" });
+  const { symbol } = req.query;
+  let returnObj = {
+    symbol,
+    timeOfQuery: new Date(),
+    currentPrice: "null",
+    previousClose: "null",
+    percentChange: "null",
+  };
+  try {
+    finnhubClient.quote(symbol, (error, data, response) => {
+      if (error) throw new Error(error);
+      returnObj.currentPrice = data.c;
+      returnObj.previousClose = data.pc;
+      returnObj.percentChange = data.dp;
+    });
+    res.send(returnObj);
+  } catch (error) {
+    res.status(404).send("Server Failure");
+  }
 });
 
 //listen for request on port 3000, and as a callback function have the port listened on logged
